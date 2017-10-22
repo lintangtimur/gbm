b4e1fded0ba3a1f19f79706c38baec2026e02df7
cep.fajar <cep.fajar@iconpln.co.id>
2017-10-22 12:02:14 +0700

    penambahan regional dan sum

--- application/modules/template/views/login.php
+++ application/modules/template/views/login.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Metro Design</title>
+        <title><?php echo $app_parameter['nama_aplikasi'];?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
