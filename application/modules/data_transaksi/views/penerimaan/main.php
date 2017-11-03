fe60023fbcd75b75c0c6da78e10c6a5a9e4e2007
cep.fajar <cep.fajar@iconpln.co.id>
2017-11-03 18:48:36 +0700

    Perbaikan proses edit

--- application/modules/data_transaksi/views/penerimaan/main.php
+++ application/modules/data_transaksi/views/penerimaan/main.php
@@ -181,12 +181,17 @@
 
                     if (vLevelUser>=2){
                         if (vLevelUser==2){
+
+                            vlink_url = "<?php echo base_url()?>data_transaksi/penerimaan/edit/"+data_detail[i].ID_PENERIMAAN;
+                            vEdit = '<a href="javascript:void(0);" class="btn transparant" id="button-edit-'+data_detail[i].ID_PENERIMAAN+'" onclick="load_form(this.id)" data-source="'+vlink_url+'"> <i class="icon-edit"></i></a>'; 
+
                             if (data_detail[i].KODE_STATUS !== "1"){
                                 cekbox = '';
-                            }   
-                            if (data_detail[i].KODE_STATUS == "0"){
-                                vEdit = '';
                             }  
+
+                            // if (data_detail[i].KODE_STATUS == "0"){
+                            //     vEdit = '';
+                            // }  
                         }
 
                         if ((vLevelUser==3) || (vLevelUser==4)){
