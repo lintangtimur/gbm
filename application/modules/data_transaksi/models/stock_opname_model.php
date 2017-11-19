95cfc2ed3b5fa59dc123694a4a191cbf80a59551
rakhmatwijayanto <rahmatwijaya11@gmail.com>
2017-11-19 12:57:20 +0700

    update so

--- application/modules/data_transaksi/models/stock_opname_model.php
+++ application/modules/data_transaksi/models/stock_opname_model.php
@@ -91,7 +91,6 @@ class stock_opname_model extends CI_Model {
         $TGL_PENGAKUAN = $myDate->format('dmY');
 
         $query="CALL SP_TEMP_SO('$ID_STOCKOPNAME', '$SLOC', '$ID_JNS_BHN_BKR', '$TGL_PENGAKUAN', '$LEVEL_USER', '$STATUS', '$USER')";
-		// print_debug($query);
         $data = $this->db->query($query);
         return $data->result();
 
@@ -110,14 +109,6 @@ class stock_opname_model extends CI_Model {
             return TRUE;
         }
     }
-    
-    
-    // versi store prosecure
-    // public function save_as_new($data) {
-    //      $query="CALL SAVE_STOCK_OPNAME('$data')";
-    //      $data = $this->db->query($query);
-    //      return $data->result();
-    //  }
 
     public function save($data, $key) {
         $this->db->trans_begin();
@@ -197,24 +188,6 @@ class stock_opname_model extends CI_Model {
                         }
                     }
                 }
-
-
-                // if($level_user == 2){
-                //     if ($status == 1) {
-                //         $aksi .= anchor(null, '<i class="icon-zoom-in" title="View"></i>', array('class' => 'btn transparant', 'id' => 'button-load-' . $id, 'onclick' => 'load_form(this.id)', 'data-source' => base_url($module . '/loadApprove/' . $id)));
-                //     }
-                //     else if(($status==2) || ($status==3) ){
-                //         $aksi .= anchor(null, '<i class="icon-zoom-in" title="View"></i>', array('class' => 'btn transparant', 'id' => 'button-load-' . $id, 'onclick' => 'load_form(this.id)', 'data-source' => base_url($module . '/loadView/' . $id)));
-                //     }
-                // } 
-                // else if (($level_user == 3)||($level_user == 4)){
-                //     if (($status == 0) || ($status == 3)) {
-                //         $aksi .= anchor(null, '<i class="icon-share" title="Kirim"></i>', array('class' => 'btn transparant', 'id' => 'button-kirim-' . $id, 'onclick' => 'kirim_row(this.id)', 'data-source' => base_url($module . '/sendAction/' . $id)));
-                //         $aksi .= anchor(null, '<i class="icon-edit" title="Edit"></i>', array('class' => 'btn transparant', 'id' => 'button-edit-' . $id, 'onclick' => 'load_form(this.id)', 'data-source' => base_url($module . '/edit/' . $id)));
-                //     }else if(($status==2)||($status==1)){
-                //         $aksi .= anchor(null, '<i class="icon-zoom-in" title="View"></i>', array('class' => 'btn transparant', 'id' => 'button-load-' . $id, 'onclick' => 'load_form(this.id)', 'data-source' => base_url($module . '/loadView/' . $id)));
-                //     }
-                // }
 
                 if($status==0){
                    $status_hasil="Belum Dikirim";
