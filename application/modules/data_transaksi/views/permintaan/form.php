03cff0419452193adbaa6c24ecb4e7ba2a39e9a2
Cep Yudi Fajar <yudy603@gmail.com>
2017-11-29 15:45:09 +0700

    Label L

--- application/modules/data_transaksi/views/permintaan/form.php
+++ application/modules/data_transaksi/views/permintaan/form.php
@@ -93,7 +93,7 @@
                   </div>
             </div>
             <div class="control-group">
-                <label class="control-label">Volume<span class="required">*</span> : </label>
+                <label class="control-label">Volume (L)<span class="required">*</span> : </label>
                 <div class="controls">
                     <?php echo form_input('VOLUME_NOMINASI', !empty($default->VOLUME_NOMINASI) ? $default->VOLUME_NOMINASI : '0', 'class="span4" placeholder="0" id="VOLUME_NOMINASI" readonly'); ?>
                 </div>
