a875eb1629277249598938d1c1f9e631eceb996a
Lintang Timur <lintangtimur@protonmail.com>
2018-02-07 10:36:48 +0700

    prod to dev

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 date_default_timezone_set('Asia/Jakarta');
 /*
  * ---------------------------------------------------------------
@@ -18,7 +19,7 @@ date_default_timezone_set('Asia/Jakarta');
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-define('ENVIRONMENT', 'production'); 
+define('ENVIRONMENT', 'development');
 /*
  * ---------------------------------------------------------------
  * ERROR REPORTING
@@ -100,7 +101,6 @@ $application_folder = 'application';
 // The controller function you wish to be called.
 // $routing['function']	= '';
 
-
 /*
  * -------------------------------------------------------------------
  *  CUSTOM CONFIG VALUES
@@ -132,7 +132,7 @@ if (defined('STDIN')) {
     chdir(dirname(__FILE__));
 }
 
-if (realpath($system_path) !== FALSE) {
+if (realpath($system_path) !== false) {
     $system_path = realpath($system_path) . '/';
 }
 
@@ -141,7 +141,7 @@ $system_path = rtrim($system_path, '/') . '/';
 
 // Is the system path correct?
 if (!is_dir($system_path)) {
-    exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: " . pathinfo(__FILE__, PATHINFO_BASENAME));
+    exit('Your system folder path does not appear to be set correctly. Please open the following file and correct this: ' . pathinfo(__FILE__, PATHINFO_BASENAME));
 }
 
 /*
@@ -157,7 +157,7 @@ define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 define('EXT', '.php');
 
 // Path to the system folder
-define('BASEPATH', str_replace("\\", "/", $system_path));
+define('BASEPATH', str_replace('\\', '/', $system_path));
 
 // Path to the front controller (this file)
 define('FCPATH', str_replace(SELF, '', __FILE__));
@@ -165,13 +165,12 @@ define('FCPATH', str_replace(SELF, '', __FILE__));
 // Name of the "system folder"
 define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
 
-
 // The path to the "application" folder
 if (is_dir($application_folder)) {
     define('APPPATH', $application_folder . '/');
 } else {
     if (!is_dir(BASEPATH . $application_folder . '/')) {
-        exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: " . SELF);
+        exit('Your application folder path does not appear to be set correctly. Please open the following file and correct this: ' . SELF);
     }
 
     define('APPPATH', BASEPATH . $application_folder . '/');
