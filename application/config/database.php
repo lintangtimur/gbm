981962bb7a55f70218b869c089318f42769e6763
Lintang Timur <lintangtimur@protonmail.com>
2018-02-07 09:53:12 +0700

    update database password

--- application/config/database.php
+++ application/config/database.php
@@ -2,6 +2,7 @@
 
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
+
 /*
   | -------------------------------------------------------------------
   | DATABASE CONNECTIVITY SETTINGS
@@ -15,31 +16,31 @@ if (!defined('BASEPATH'))
   | EXPLANATION OF VARIABLES
   | -------------------------------------------------------------------
   |
-  |	['hostname'] The hostname of your database server.
-  |	['username'] The username used to connect to the database
-  |	['password'] The password used to connect to the database
-  |	['database'] The name of the database you want to connect to
-  |	['dbdriver'] The database type. ie: mysql.  Currently supported:
+  | ['hostname'] The hostname of your database server.
+  | ['username'] The username used to connect to the database
+  | ['password'] The password used to connect to the database
+  | ['database'] The name of the database you want to connect to
+  | ['dbdriver'] The database type. ie: mysql.  Currently supported:
   mysql, mysqli, postgre, odbc, mssql, sqlite, oci8
-  |	['dbprefix'] You can add an optional prefix, which will be added
-  |				 to the table name when using the  Active Record class
-  |	['pconnect'] TRUE/FALSE - Whether to use a persistent connection
-  |	['db_debug'] TRUE/FALSE - Whether database errors should be displayed.
-  |	['cache_on'] TRUE/FALSE - Enables/disables query caching
-  |	['cachedir'] The path to the folder where cache files should be stored
-  |	['char_set'] The character set used in communicating with the database
-  |	['dbcollat'] The character collation used in communicating with the database
-  |				 NOTE: For MySQL and MySQLi databases, this setting is only used
-  | 				 as a backup if your server is running PHP < 5.2.3 or MySQL < 5.0.7
-  |				 (and in table creation queries made with DB Forge).
-  | 				 There is an incompatibility in PHP with mysql_real_escape_string() which
-  | 				 can make your site vulnerable to SQL injection if you are using a
-  | 				 multi-byte character set and are running versions lower than these.
-  | 				 Sites using Latin-1 or UTF-8 database character set and collation are unaffected.
-  |	['swap_pre'] A default table prefix that should be swapped with the dbprefix
-  |	['autoinit'] Whether or not to automatically initialize the database.
-  |	['stricton'] TRUE/FALSE - forces 'Strict Mode' connections
-  |							- good for ensuring strict SQL while developing
+  | ['dbprefix'] You can add an optional prefix, which will be added
+  |        to the table name when using the  Active Record class
+  | ['pconnect'] TRUE/FALSE - Whether to use a persistent connection
+  | ['db_debug'] TRUE/FALSE - Whether database errors should be displayed.
+  | ['cache_on'] TRUE/FALSE - Enables/disables query caching
+  | ['cachedir'] The path to the folder where cache files should be stored
+  | ['char_set'] The character set used in communicating with the database
+  | ['dbcollat'] The character collation used in communicating with the database
+  |        NOTE: For MySQL and MySQLi databases, this setting is only used
+  |          as a backup if your server is running PHP < 5.2.3 or MySQL < 5.0.7
+  |        (and in table creation queries made with DB Forge).
+  |          There is an incompatibility in PHP with mysql_real_escape_string() which
+  |          can make your site vulnerable to SQL injection if you are using a
+  |          multi-byte character set and are running versions lower than these.
+  |          Sites using Latin-1 or UTF-8 database character set and collation are unaffected.
+  | ['swap_pre'] A default table prefix that should be swapped with the dbprefix
+  | ['autoinit'] Whether or not to automatically initialize the database.
+  | ['stricton'] TRUE/FALSE - forces 'Strict Mode' connections
+  |             - good for ensuring strict SQL while developing
   |
   | The $active_group variable lets you choose which connection group to
   | make active.  By default there is only one group (the 'default' group).
@@ -52,13 +53,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = '10.14.152.87';
- // $db['default']['hostname'] = '10.1.18.206';
 $db['default']['username'] = 'gbmon';
-$db['default']['password'] = 'VvBZh8ZETswV6FjD';
-// $db['default']['password'] = '';
-// $db['default']['password'] = 'nicky.astria';
+$db['default']['password'] = 'J4N64NLUP4L461';
 $db['default']['database'] = 'gbm_new_dev';
-// $db['default']['database'] = 'gbm_prod';
+
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['port']     = 3306;
@@ -70,8 +68,7 @@ $db['default']['char_set'] = 'utf8';
 $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
-
+$db['default']['stricton'] = FALSE;  
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
