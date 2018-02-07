a0ef00a3c5b1e80068bccc3a469e8218cb581048
Lintang Timur <lintangtimur@protonmail.com>
2018-02-07 11:15:29 +0700

    update array notation

--- application/modules/dashboard/controllers/dashboard.php
+++ application/modules/dashboard/controllers/dashboard.php
@@ -24,31 +24,31 @@ class dashboard extends MX_Controller
         // Load Modules
         $this->load->module('template/asset');
         // Memanggil plugin JS Crud
-        $this->asset->set_plugin(['crud', 'highchart']);
-        $data['button_group'] = [
-            anchor(null, '<i class="icon-plus"></i> Tambah Data', ['class' => 'btn yellow', 'id' => 'button-add', 'onclick' => 'load_form_modal(this.id)', 'data-source' => base_url($this->_module . '/add')])
-        ];
+        $this->asset->set_plugin(array('crud', 'highchart'));
+        $data['button_group'] = array(
+            anchor(null, '<i class="icon-plus"></i> Tambah Data', array('class' => 'btn yellow', 'id' => 'button-add', 'onclick' => 'load_form_modal(this.id)', 'data-source' => base_url($this->_module . '/add')))
+        );
         $data['sidebar_content']    = 'dashboard/sidebar';
-        $data['motivasi']           = [];
+        $data['motivasi']           = array();
         $data['page_title']         = '<i class="icon-home"></i> ' . $this->_title;
         $data['page_content']       = $this->_module . '/main2';
-        $data['source_search']      = [];
-        $data['sources_ate']        = [];
-        $data['sources_taks']       = [];
-        $data['sources_k_internal'] = [];
-        $data['sources_elibrary']   = [];
-        $data['sources_info']       = [];
+        $data['source_search']      = array();
+        $data['sources_ate']        = array();
+        $data['sources_taks']       = array();
+        $data['sources_k_internal'] = array();
+        $data['sources_elibrary']   = array();
+        $data['sources_info']       = array();
 
-        $data['suggest_elibrary']    = [];
-        $data['suggest_elibrary2']   = [];
-        $data['suggest_elibrary3']   = [];
+        $data['suggest_elibrary']    = array();
+        $data['suggest_elibrary2']   = array();
+        $data['suggest_elibrary3']   = array();
 
-        $data['suggest_takso1']       = [];
-        $data['suggest_takso2']       = [];
+        $data['suggest_takso1']       = array();
+        $data['suggest_takso2']       = array();
 
-        $data['picbelumlaporan'] = [];
-        $data['picsudahlaporan'] = [];
-        $data['belumverifikasi'] = [];
+        $data['picbelumlaporan'] = array();
+        $data['picsudahlaporan'] = array();
+        $data['belumverifikasi'] = array();
 
         echo Modules::run('template/admin', $data);
     }
