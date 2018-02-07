28162f9a8d7e231d3498ef2751ee42a6a937a05c
Lintang Timur <lintangtimur@protonmail.com>
2018-02-07 10:43:17 +0700

    php formater

--- application/modules/dashboard/controllers/dashboard.php
+++ application/modules/dashboard/controllers/dashboard.php
@@ -1,15 +1,17 @@
 <?php
 
-if (!defined("BASEPATH")) {
-    exit("No direct script access allowed");
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
 }
 
-class dashboard extends MX_Controller {
-
-    private $_title = 'Dashboard';
+class dashboard extends MX_Controller
+{
+    private $_title  = 'Dashboard';
     private $_module = 'dashboard';
-     private $_limit = 5;
-    public function __construct() {
+    private $_limit  = 5;
+
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('dashboard_model');
 
@@ -17,39 +19,39 @@ class dashboard extends MX_Controller {
         hprotection::login();
     }
 
-    public function index() {
+    public function index()
+    {
         // Load Modules
-        $this->load->module("template/asset");
-         // Memanggil plugin JS Crud
-        $this->asset->set_plugin(array('crud','highchart'));
-        $data['button_group'] = array(
-            anchor(null, '<i class="icon-plus"></i> Tambah Data', array('class' => 'btn yellow', 'id' => 'button-add', 'onclick' => 'load_form_modal(this.id)', 'data-source' => base_url($this->_module . '/add')))
-        );
+        $this->load->module('template/asset');
+        // Memanggil plugin JS Crud
+        $this->asset->set_plugin(['crud', 'highchart']);
+        $data['button_group'] = [
+            anchor(null, '<i class="icon-plus"></i> Tambah Data', ['class' => 'btn yellow', 'id' => 'button-add', 'onclick' => 'load_form_modal(this.id)', 'data-source' => base_url($this->_module . '/add')])
+        ];
         $data['sidebar_content']    = 'dashboard/sidebar';
-        $data['motivasi']           = array();
+        $data['motivasi']           = [];
         $data['page_title']         = '<i class="icon-home"></i> ' . $this->_title;
-        $data['page_content']       = $this->_module .'/main2';
-        $data['source_search']      = array();
-        $data['sources_ate']        = array();
-        $data['sources_taks']       = array();
-        $data['sources_k_internal'] = array();
-        $data['sources_elibrary']   = array();
-        $data['sources_info']       = array();
-        
-        $data['suggest_elibrary']       = array();
-        $data['suggest_elibrary2']       = array();
-        $data['suggest_elibrary3']       = array();
-        
-        $data['suggest_takso1']       = array();
-        $data['suggest_takso2']       = array();
-		
-		$data['picbelumlaporan'] = array();
-		$data['picsudahlaporan'] = array();
-		$data['belumverifikasi'] = array();
+        $data['page_content']       = $this->_module . '/main2';
+        $data['source_search']      = [];
+        $data['sources_ate']        = [];
+        $data['sources_taks']       = [];
+        $data['sources_k_internal'] = [];
+        $data['sources_elibrary']   = [];
+        $data['sources_info']       = [];
 
-        echo Modules::run("template/admin", $data);
+        $data['suggest_elibrary']    = [];
+        $data['suggest_elibrary2']   = [];
+        $data['suggest_elibrary3']   = [];
+
+        $data['suggest_takso1']       = [];
+        $data['suggest_takso2']       = [];
+
+        $data['picbelumlaporan'] = [];
+        $data['picsudahlaporan'] = [];
+        $data['belumverifikasi'] = [];
+
+        echo Modules::run('template/admin', $data);
     }
-    
 }
 
 /* End of file dashboard.php */
---
+++ application/modules/master/models/laporan_penerimaan_model.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * laporan penerimaan model
+ */
+class laporan_penerimaan_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
